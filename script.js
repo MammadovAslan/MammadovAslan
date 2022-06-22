a525ced046526dbc23b314295d61414913117399
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-06-22 22:14:13 +0400

    Guess the number game

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function calc(){
     let x = document.getElementById('x').value;
-    let y = document.getElementById('y').value;
-    let hypotenuse = Math.sqrt(x**2 + y**2)
-    return document.getElementById('result').innerHTML = hypotenuse.toFixed(4)
+    let num = Math.floor(Math.random()*10)
+    let res = 0
+    if(x == num){
+        res = 'You Win!'
+    }else if(x<num){
+        res = 'Pick higher Number'
+    }else if(x>num){
+        res = 'Pick smaller number'
+    }
+
+    return document.getElementById('result').innerHTML = res
 }
+
 
 
