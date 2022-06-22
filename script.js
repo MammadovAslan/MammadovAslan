a58e63b51cb45efb4cafbe6560eadaf150c861d1
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-06-22 21:29:07 +0400

    pythagoras theorem

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//code here
+function calc(){
+    let x = document.getElementById('x').value;
+    let y = document.getElementById('y').value;
+    let hypotenuse = Math.sqrt(x**2 + y**2)
+    return document.getElementById('result').innerHTML = hypotenuse.toFixed(4)
+}
+
+
